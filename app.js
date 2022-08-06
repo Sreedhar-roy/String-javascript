e5e4bc3a59c076123b9d94db7ea2991ad5f09e6f
Sreedhar-Roy <sreedharroy0@gmail.com>
2022-08-07 00:40:43 +0600

    font family commit

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+// const userName = 'Sreedhar-roy';
+// const userInput = 'sreedhar-roy';
+
+// if(userName === userInput){
+//     console.log('valid input name')
+// }
+// else{
+//     console.log('invalid user name');
+// }
+
 const userName = 'Sreedhar-roy';
 const userInput = 'sreedhar-roy';
 
-if(userName === userInput){
+console.log(userName.toLowerCase());
+console.log(userInput.toUpperCase());
+console.log(userName.toLocaleLowerCase());
+if(userName.toLowerCase() === userInput.toLowerCase()){
     console.log('valid input name')
 }
 else{
