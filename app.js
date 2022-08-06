dba53e8f286759376c28229dd051bfef269dd066
Sreedhar-Roy <sreedharroy0@gmail.com>
2022-08-07 00:26:49 +0600

    first commit javascript of string

---
+++ app.js
@@ -0,0 +1,9 @@
+const userName = 'Sreedhar-roy';
+const userInput = 'sreedhar-roy';
+
+if(userName === userInput){
+    console.log('valid input name')
+}
+else{
+    console.log('invalid user name');
+}
