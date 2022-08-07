10a9c5626ca5b7d6f40e457aa636aa665571170a
Sreedhar-Roy <sreedharroy0@gmail.com>
2022-08-07 10:14:32 +0600

    doesnotExits commit

--- search.js
+++ search.js
@@ -1,3 +1,5 @@
 const lyrics = 'tumi bondhu kala pakhi ami jno ki . bosonto kale tomay bolte parini. sada sada kala kala rong jomeche sada kala';
 const doesExist =lyrics.includes('pakhi');
+const doesNotExist = lyrics.includes('Tomay');
 console.log(doesExist);
+console.log(doesNotExist);
