f5da7485c0060d84f9d69de9ccc41344f299395b
Sreedhar-Roy <sreedharroy0@gmail.com>
2022-08-08 01:05:21 +0600

    reverse way in blog

---
+++ reverse.js
@@ -0,0 +1,15 @@
+function reversestring(text){
+    for(i=text.length-1; i>=0; i--){
+        const element=text[i];
+        console.log(element);
+    }
+}
+const mystring = 'i am a good boy';
+const reversed = reversestring(mystring);
+
+// simple reversed way 
+const text2 = 'bangladesh amar desh'
+for(i=text2.length-1; i>=0; i--){
+    const element2 = text2[i];
+    console.log(element2);
+}
