345819423b32945608c9e099027729b4e50189c5
Sreedhar-Roy <sreedharroy0@gmail.com>
2022-08-07 12:45:54 +0600

    split commit

---
+++ split-join.js
@@ -0,0 +1,10 @@
+const lyrics = "ami valo vabei sob kichu bujhe bujhe kaj korte chai. jdi ami parbo se din ami o hoyto onek vabei manush ke bujhabo. atai ar asha";
+
+// const parts = lyrics.split(" ")
+// console.log(parts);
+
+// const sentence = lyrics.split('.');
+// console.log(sentence);
+
+const chars = lyrics.split('');
+console.log(chars);
