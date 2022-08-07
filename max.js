c1d2690d5947e03143691b02d614548a1d373a44
Sreedhar-Roy <sreedharroy0@gmail.com>
2022-08-07 21:50:10 +0600

    add the math min and max number

---
+++ max.js
@@ -0,0 +1,24 @@
+const dela =67;
+const mila =87;
+const chinku = 97;
+if(dela >mila && dela> chinku){
+    console.log('dela will get cake');
+}
+else if(mila> dela && mila> chinku){
+    console.log('mila will ger the cake bela');
+}
+else{
+    console.log('chinku is a great person and get will the full cake')
+}
+
+// ===================max and min 
+let a = 102;
+let b = 203;
+let c = 23;
+let d = 56;
+
+let min = Math.min(a, b, c, d);
+console.log(min);
+
+let max = Math.max(a, b, c, d);
+console.log(max);
