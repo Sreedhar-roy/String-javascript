959ffd0e9b39f57dc745fd04d6736c31d197cb90
Sreedhar-Roy <sreedharroy0@gmail.com>
2022-08-07 10:06:55 +0600

    search lyrics commit

---
+++ search.js
@@ -0,0 +1,3 @@
+const lyrics = 'tumi bondhu kala pakhi ami jno ki . bosonto kale tomay bolte parini. sada sada kala kala rong jomeche sada kala';
+const doesExist =lyrics.includes('pakhi');
+console.log(doesExist);
