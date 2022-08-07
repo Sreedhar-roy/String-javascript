24c02bb124d946e5ace5780effeecc6ac8145437
Sreedhar-Roy <sreedharroy0@gmail.com>
2022-08-07 21:00:11 +0600

    swap commit

---
+++ swap.js
@@ -0,0 +1,11 @@
+let first =8;
+let second = 9;
+console.log(first, second);
+// const temp = first;
+//     first = second;
+//     second = temp;
+// console.log(first, second);
+
+// approse:2 ---distructuring ruls
+[first , second] =[second , first];
+console.log(first, second);
